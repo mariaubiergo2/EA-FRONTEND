ecb3c80501d8fdbb7d3363de74443dc9153a53ad
Marcel <marcel.marco@estudiantat.upc.edu>
2023-05-13 10:15:49 +0200

    Mapa Visualizable

--- lib/pages/initial_screen.dart
+++ lib/pages/initial_screen.dart
@@ -4,7 +4,10 @@ import '../widget/maps_widget.dart';
 import '../widget/panel_widget.dart';
 import 'package:ea_frontend/pages/navbar.dart';
 import 'package:dio/dio.dart';
-
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InitialScreen extends StatefulWidget {
   //const LoginScreen({super.key, required String title});
@@ -35,7 +38,29 @@ class _InitialScreenState extends State<InitialScreen> {
             minHeight: panelHeightClosed,
             parallaxEnabled: true,
             parallaxOffset: .5,
-            body: const MapsWidget(),
+            body: FlutterMap(
+              options: MapOptions(
+                center: LatLng(41.27561, 1.98722),
+                zoom: 16.0,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                ),
+              ],
+              nonRotatedChildren: [
+                RichAttributionWidget(
+                  attributions: [
+                    TextSourceAttribution(
+                      'OpenStreetMap contributors',
+                      onTap: () => launchUrl(
+                          Uri.parse('https://openstreetmap.org/copyright')),
+                    ),
+                  ],
+                ),
+              ],
+            ),
             panelBuilder: (controller) => PanelWidget(
               controller: controller,
               panelController: panelController,
