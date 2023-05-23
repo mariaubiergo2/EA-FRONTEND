3e32549991f9721c918a7f5e1cdac09429a68082
PabloGarciaCaldero <pablo-.maria.garcia@estudiantat.upc.edu>
2023-05-23 20:11:29 +0200

    primera aproximació

--- lib/widget/maps_widget.dart
+++ lib/widget/maps_widget.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_interpolation_to_compose_strings
 
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -8,6 +9,8 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ea_frontend/models/challenge.dart';
 import 'package:dio/dio.dart';
+
+import '../pages/challenge_screen.dart';
 
 void main() async {
   await dotenv.load();
@@ -79,7 +82,113 @@ class MapsWidget extends State<MapScreen> {
     setState(() {
       challengeList = challengeList;
     });
-    fetchAndBuildMarkers();
+    fetchAndBuildMarkers3();
+  }
+
+  Future<void> fetchAndBuildMarkers3() async {
+    final newMarkers = challengeList.map((challenge) {
+      final lat = double.parse(challenge.lat);
+      final long = double.parse(challenge.long);
+      final snackBar =
+          SnackBar(content: Text("Este reto es: " + challenge.name));
+      return Marker(
+        point: LatLng(lat, long),
+        builder: (context) => GestureDetector(
+          onTap: () {
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context); // Cerrar la pantalla actual
+                  },
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+                    child: Container(
+                      color: Colors.black.withOpacity(0.5),
+                      child: const MyChallengePage(),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+          child: Image.asset('images/marker.png'),
+        ),
+      );
+    }).toList();
+
+    setState(() {
+      allmarkers = newMarkers;
+    });
+  }
+
+  Future<void> fetchAndBuildMarkers2() async {
+    final newMarkers = challengeList.map((challenge) {
+      final lat = double.parse(challenge.lat);
+      final long = double.parse(challenge.long);
+      final snackBar =
+          SnackBar(content: Text("Este reto es: " + challenge.name));
+      return Marker(
+        point: LatLng(lat, long),
+        builder: (context) => GestureDetector(
+          onTap: () {
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            showDialog(
+              context: context,
+              barrierColor: Colors.black.withOpacity(0.5),
+              builder: (BuildContext context) {
+                return Stack(
+                  children: [
+                    // Fondo difuminado
+                    BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+                      child: Container(
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                    ),
+                    // Página de desafío
+                    MyChallengePage(),
+                  ],
+                );
+              },
+            );
+          },
+          child: Image.asset('images/marker.png'),
+        ),
+      );
+    }).toList();
+
+    setState(() {
+      allmarkers = newMarkers;
+    });
+  }
+
+  Future<void> fetchAndBuildMarkers4() async {
+    final newMarkers = challengeList.map((challenge) {
+      final lat = double.parse(challenge.lat);
+      final long = double.parse(challenge.long);
+      final snackBar =
+          SnackBar(content: Text("Este reto es: " + challenge.name));
+      return Marker(
+        point: LatLng(lat, long),
+        builder: (context) => GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(
+              context,
+              '/challenge_screen',
+              arguments: challenge,
+            );
+          },
+          child: Image.asset('images/marker.png'),
+        ),
+      );
+    }).toList();
+
+    setState(() {
+      allmarkers = newMarkers;
+    });
   }
 
   Future<void> fetchAndBuildMarkers() async {
@@ -94,6 +203,9 @@ class MapsWidget extends State<MapScreen> {
         builder: (context) => GestureDetector(
           onTap: () {
             ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            // Navigator.pushNamed(context, '/dialog_helper');
+            //DialogHelper.exit(context);
+            //Navigator.of(context).pop(false);
           },
           child: Image.asset('images/marker.png'),
         ),
