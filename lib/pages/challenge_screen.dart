af1d68329895fd9e0848ffa7757faa6611664c81
PabloGarciaCaldero <pablo-.maria.garcia@estudiantat.upc.edu>
2023-05-26 20:33:06 +0200

    ja fucnionen les cartes, falta disseny

--- lib/pages/challenge_screen.dart
+++ lib/pages/challenge_screen.dart
@@ -1,7 +1,40 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class MyChallengePage extends StatelessWidget {
-  const MyChallengePage({super.key});
+import '../models/challenge.dart';
+
+class MyChallengePage extends StatefulWidget {
+  final String? selectedChallengeId;
+  final String? nameChallenge;
+  final String? descrChallenge;
+  const MyChallengePage(
+      {Key? key,
+      this.selectedChallengeId,
+      this.nameChallenge,
+      this.descrChallenge})
+      : super(key: key);
+
+  @override
+  State<MyChallengePage> createState() => _MyChallengePageState();
+}
+
+class _MyChallengePageState extends State<MyChallengePage> {
+  Challenge? challenge;
+  String? _token = "";
+  String? _idChallenge = "";
+  String? _name = "";
+  String? _descr = "";
+  String? _exp = "";
+
+  @override
+  void initState() {
+    super.initState();
+    getChallengeInfo().then((_) {
+      callApi();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,22 +46,62 @@ class MyChallengePage extends StatelessWidget {
     );
   }
 
-  _buildChild(BuildContext context) => Container(
+  Future<void> getChallengeInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _token = prefs.getString('token');
+      _idChallenge = widget.selectedChallengeId;
+      _name = widget.nameChallenge;
+      _descr = widget.descrChallenge;
+      _exp = prefs.getString('exp');
+    });
+  }
+
+  Future<void> callApi() async {
+    String path =
+        'http://${dotenv.env['API_URL']}/challenge/get/${widget.selectedChallengeId}';
+    print(path);
+    var response = await Dio().get(
+      path,
+      options: Options(
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer $_token",
+        },
+      ),
+    );
+    var challengeData = response.data;
+    var challenge = Challenge.fromJson(challengeData);
+    setState(() {
+      this.challenge = challenge;
+    });
+  }
+
+  Widget _buildChild(BuildContext context) {
+    if (challenge == null) {
+      // Muestra un indicador de carga mientras se obtiene la información del desafío
+      return CircularProgressIndicator();
+    } else {
+      // Muestra la información del desafío
+      return Container(
         height: 350,
-        decoration: const BoxDecoration(
-            color: Colors.redAccent,
-            shape: BoxShape.rectangle,
-            borderRadius: BorderRadius.all(Radius.circular(12))),
+        decoration: BoxDecoration(
+          color: Colors.redAccent,
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
         child: Column(
           children: <Widget>[
             Container(
               width: double.infinity,
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      topRight: Radius.circular(12))),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                ),
+              ),
               child: Padding(
                 padding: const EdgeInsets.all(12.0),
                 child: Image.asset(
@@ -38,54 +111,34 @@ class MyChallengePage extends StatelessWidget {
                 ),
               ),
             ),
-            const SizedBox(
-              height: 24,
-            ),
-            const Text(
-              'Do you want to exit?',
+            SizedBox(height: 24),
+            Text(
+              _name ?? '',
               style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            const SizedBox(
-              height: 8,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(right: 16, left: 16),
+            SizedBox(height: 8),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Text(
-                'If back button is pressed by mistake then click on no to continue.',
+                _descr ?? '',
                 style: TextStyle(color: Colors.white),
                 textAlign: TextAlign.center,
               ),
             ),
-            const SizedBox(
-              height: 24,
-            ),
+            SizedBox(height: 24),
             Row(
               mainAxisSize: MainAxisSize.min,
-              children: const <Widget>[
-                // FlatButton(
-                //   onPressed: () {
-                //     Navigator.of(context).pop();
-                //   },
-                //   child: const Text('No'),
-                //   textColor: Colors.white,
-                // ),
-                SizedBox(
-                  width: 8,
-                ),
-                // RaisedButton(
-                //   onPressed: () {
-                //     return Navigator.of(context).pop(true);
-                //   },
-                //   child: const Text('Yes'),
-                //   color: Colors.white,
-                //   textColor: Colors.redAccent,
-                // )
+              children: <Widget>[
+                SizedBox(width: 8),
               ],
-            )
+            ),
           ],
         ),
       );
+    }
+  }
 }
