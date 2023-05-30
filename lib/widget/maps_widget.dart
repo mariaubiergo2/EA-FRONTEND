9fc1d35be0e02f17c30230303db2fea1c447d1e8
Adrián <adquiros15@outlook.com>
2023-05-31 00:26:42 +0200

    android, info, grandle modified & gps working

--- lib/widget/maps_widget.dart
+++ lib/widget/maps_widget.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_interpolation_to_compose_strings
-
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -8,14 +6,14 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ea_frontend/models/challenge.dart';
 import 'package:dio/dio.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() async {
   await dotenv.load();
 }
 
 class MapScreen extends StatefulWidget {
-  //const LoginScreen({super.key, required String title});
-  const MapScreen({super.key});
+  const MapScreen({Key? key}) : super(key: key);
 
   @override
   State<MapScreen> createState() => MapsWidget();
@@ -25,11 +23,89 @@ class MapsWidget extends State<MapScreen> {
   List<Marker> allmarkers = [];
   Challenge? challenge;
   List<Challenge> challengeList = <Challenge>[];
+  bool showUserLocation = false;
+  Position? userLocation;
 
   @override
   void initState() {
     super.initState();
     getChallenges();
+    getLocationPermission();
+    listenToLocationUpdates();
+  }
+
+  Future<void> getLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return;
+    }
+  }
+
+  void listenToLocationUpdates() {
+    Geolocator.getPositionStream().listen((Position position) {
+      setState(() {
+        userLocation = position;
+        showUserLocation = true;
+        updateMarkers();
+      });
+    });
+  }
+
+  void updateMarkers() {
+    allmarkers.clear();
+    if (showUserLocation && userLocation != null) {
+      final userMarker = Marker(
+        rotate: false,
+        point: LatLng(userLocation!.latitude, userLocation!.longitude),
+        builder: (_) => const Icon(
+          Icons.location_searching_rounded,
+          color: Color.fromARGB(255, 23, 126, 209),
+          size: 20,
+        ),
+      );
+      allmarkers.add(userMarker);
+    }
+    buildChallengeMarkers();
+  }
+
+  void buildChallengeMarkers() {
+    final newMarkers = challengeList.map((challenge) {
+      final lat = double.parse(challenge.lat);
+      final long = double.parse(challenge.long);
+      final snackBar =
+          SnackBar(content: Text("Este reto es: " + challenge.name));
+      return Marker(
+        point: LatLng(lat, long),
+        rotate: true,
+        builder: (context) => GestureDetector(
+          onTap: () {
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          },
+          child: const Icon(
+            Icons.location_pin,
+            color: Color.fromARGB(255, 222, 66, 66),
+            size: 25,
+          ),
+        ),
+      );
+    }).toList();
+
+    allmarkers.addAll(newMarkers);
   }
 
   @override
@@ -58,12 +134,12 @@ class MapsWidget extends State<MapScreen> {
         ),
         MarkerLayer(
           markers: allmarkers,
-        )
+        ),
       ],
     );
   }
 
-  Future getChallenges() async {
+  Future<void> getChallenges() async {
     final prefs = await SharedPreferences.getInstance();
     final String token = prefs.getString('token') ?? "";
     String path = 'http://${dotenv.env['API_URL']}/challenge/get/all';
@@ -79,30 +155,6 @@ class MapsWidget extends State<MapScreen> {
     setState(() {
       challengeList = challengeList;
     });
-    fetchAndBuildMarkers();
-  }
-
-  Future<void> fetchAndBuildMarkers() async {
-    // final challenges = await fetchChallenges();
-    final newMarkers = challengeList.map((challenge) {
-      final lat = double.parse(challenge.lat);
-      final long = double.parse(challenge.long);
-      final snackBar =
-          SnackBar(content: Text("Este reto es: " + challenge.name));
-      return Marker(
-        point: LatLng(lat, long),
-        rotate: true,
-        builder: (context) => GestureDetector(
-          onTap: () {
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          },
-          child: Image.asset('images/pointer.png'),
-        ),
-      );
-    }).toList();
-
-    setState(() {
-      allmarkers = newMarkers;
-    });
+    buildChallengeMarkers();
   }
 }
