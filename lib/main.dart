0a2ff18af9dbf857d32bfe59aa9bd416757fd82c
marioRelajao <101522382+marioRelajao@users.noreply.github.com>
2023-06-09 18:56:38 +0200

    Added light/dark themes support

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
+
 import 'package:ea_frontend/pages/credential_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:ea_frontend/pages/navbar.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'package:ea_frontend/pages/challenge_screen.dart';
 import 'package:ea_frontend/pages/profile_screen/friends_screen.dart';
+import 'package:ea_frontend/pages/home_screen/home_screen.dart';
 import 'package:ea_frontend/pages/credential_screen/login_screen.dart';
 import 'package:ea_frontend/pages/home_screen/qr_screen.dart';
 import 'package:ea_frontend/pages/profile_screen/makefriends_screen.dart';
 import 'package:ea_frontend/pages/credential_screen/register_screen.dart';
+import 'package:ea_frontend/pages/credential_screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:ea_frontend/pages/navbar.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   await dotenv.load();
   runApp(const MyApp());
 }
@@ -29,6 +30,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'EETAC Go',
+        theme: ThemeData.light().copyWith(
+          brightness: Brightness.light,
+          backgroundColor: Color.fromARGB(255, 242, 242, 242),
+          dividerColor: Color.fromARGB(255, 25, 25, 25),
+          buttonTheme: ButtonThemeData(
+              buttonColor: Color.fromARGB(255, 222, 66, 66),
+              textTheme: ButtonTextTheme.primary),
+          textTheme: TextTheme(
+            bodyText1: TextStyle(
+              color: Color.fromARGB(255, 25, 25, 25),
+            ),
+            headline6: TextStyle(
+              color: Colors.red,
+            ),
+          ),
+        ),
+        darkTheme: ThemeData.dark().copyWith(
+          brightness: Brightness.dark,
+          backgroundColor: Color.fromARGB(255, 25, 25, 25),
+          dividerColor: Color.fromARGB(255, 242, 242, 242),
+          textTheme: TextTheme(
+            bodyText1: TextStyle(
+              color: Color.fromARGB(255, 242, 242, 242),
+            ),
+            headline6: TextStyle(
+              color: Colors.red,
+            ),
+          ),
+          // Other dark theme properties
+        ),
+        themeMode: ThemeMode.system,
         home: const SplashScreen(),
         onGenerateRoute: (RouteSettings settings) {
           switch (settings.name) {
