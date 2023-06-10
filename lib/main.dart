5fec990b9c23ac93a1c799d9e8b2fd9a72d21449
PabloGarciaCaldero <pablo-.maria.garcia@estudiantat.upc.edu>
2023-06-10 17:18:28 +0200

    perfil solucionado

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,13 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await dotenv.load();
   runApp(const MyApp());
 }
