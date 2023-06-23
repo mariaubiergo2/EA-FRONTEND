5b31c7cae1bd92bbde72318285d2cd5385e79e20
marioRelajao <mario.cerda@estudiantat.upc.edu>
2023-06-23 19:54:48 +0200

    Initial Commit - Image Upload Fire Storage

--- lib/mobile/credential_screen/register_screen.dart
+++ lib/mobile/credential_screen/register_screen.dart
@@ -173,7 +173,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   borderRadius: BorderRadius.circular(10),
                 ),
                 margin: const EdgeInsets.fromLTRB(20, 0, 20, 22.5),
-                content: const Row(
+                content: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Expanded(
