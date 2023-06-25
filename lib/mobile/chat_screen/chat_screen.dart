b4bdb9191473be92b76a412a8422dc44e088c9f5
PabloGarciaCaldero <pablo-.maria.garcia@estudiantat.upc.edu>
2023-06-25 12:43:57 +0200

    falta corregir error

--- lib/mobile/chat_screen/chat_screen.dart
+++ lib/mobile/chat_screen/chat_screen.dart
@@ -3,6 +3,7 @@ import 'package:chat_bubbles/message_bars/message_bar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:ea_frontend/models/user.dart';
+import 'package:ea_frontend/widget/loading_circle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/intl.dart';
@@ -47,7 +48,7 @@ class _ChatWidgetState extends State<ChatWidget> {
   Map<String, String> roomNames = {};
   IO.Socket? socket;
   String _currentRoom = '';
-  String profilePic= " ";
+  String profilePic = " ";
   List<ChatMessage> _messages = [];
   final TextEditingController _textController = TextEditingController();
 
@@ -55,7 +56,7 @@ class _ChatWidgetState extends State<ChatWidget> {
   void initState() {
     super.initState();
     socket = widget.socketWidget;
-    roomNames = widget.roomNamesWidget!;   
+    roomNames = widget.roomNamesWidget!;
     createRoom(widget.roomNameWidget!);
     loadMessages();
   }
@@ -71,7 +72,6 @@ class _ChatWidgetState extends State<ChatWidget> {
 
     print("//////////////////////////////////////// URL: ");
     print(profilePic);
-      
   }
 
   void createRoom(String roomName) {
@@ -101,110 +101,114 @@ class _ChatWidgetState extends State<ChatWidget> {
     }
   }
 
+  String changeDateTimeFormat(DateTime date) {
+    final DateFormat formatter = DateFormat('yyyy-MM-dd | hh:mm');
+    final String formatted = formatter.format(date);
+    return formatted;
+  }
 
+  Widget doMessageBubble(ChatMessage message, String username) {
+    print("//////////////////////////////////////// Url sender: ");
+    print(message.photoURL);
+    final isSender = message.senderName == username;
+    final visibility = !isSender;
+    final alignment =
+        isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start;
+    final alignment2 =
+        isSender ? MainAxisAlignment.end : MainAxisAlignment.start;
 
-String changeDateTimeFormat (DateTime date){
-  final DateFormat formatter = DateFormat('yyyy-MM-dd | hh:mm');
-  final String formatted = formatter.format(date);
-  return formatted;
-}
-
-Widget doMessageBubble(ChatMessage message, String username) {
-  print("//////////////////////////////////////// Url sender: ");
-  print(message.photoURL);
-  final isSender = message.senderName == username;
-  final visibility = !isSender;
-  final alignment = isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start;
-  final alignment2 = isSender ? MainAxisAlignment.end : MainAxisAlignment.start;
-
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: alignment,
-    children: [
-      Padding(
-        padding: const EdgeInsets.fromLTRB(70, 0, 4, 0),
-        child: Visibility(
-          visible: visibility,
-          child: Text(
-            message.senderName,
-            style: const TextStyle(
-              fontSize: 16.0,
-              color: Colors.amber,
-              fontWeight: FontWeight.bold,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: alignment,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(70, 0, 4, 0),
+          child: Visibility(
+            visible: visibility,
+            child: Text(
+              message.senderName,
+              style: const TextStyle(
+                fontSize: 16.0,
+                color: Colors.amber,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
-      ),
-      Row(
-        mainAxisAlignment: alignment2,
-        crossAxisAlignment: alignment,
-        children: [
-          Visibility(
-            visible: visibility,
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(9, 20, 0, 0),
-              child: CircleAvatar(
-                radius: 20,
-                backgroundImage: message.photoURL != ""
-                    ? Image.network(message.photoURL).image
-                    : const AssetImage('images/default.png'),
+        Row(
+          mainAxisAlignment: alignment2,
+          crossAxisAlignment: alignment,
+          children: [
+            Visibility(
+              visible: visibility,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(9, 20, 0, 0),
+                child: CircleAvatar(
+                  radius: 20,
+                  backgroundImage: message.photoURL != ""
+                      ? Image.network(message.photoURL).image
+                      : const AssetImage('images/default.png'),
+                ),
               ),
             ),
-          ),
-          Column(
-            mainAxisAlignment: alignment2,
-            crossAxisAlignment: alignment,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 2, 0, 4),
-                child: BubbleSpecialThree(
-                  text: message.messageContent,
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  tail: true,
-                  isSender: isSender,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(18, 2, 18, 4),
-                child: Text(
-                  changeDateTimeFormat(message.timeSent.toDate()),
-                  style: const TextStyle(
-                    fontSize: 13.0,
-                    color: Colors.grey,
+            Column(
+              mainAxisAlignment: alignment2,
+              crossAxisAlignment: alignment,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 2, 0, 4),
+                  child: BubbleSpecialThree(
+                    text: message.messageContent,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    tail: true,
+                    isSender: isSender,
                   ),
                 ),
-              ),              
-            ],
-          ),
-        ],
-      ),
-    ],
-  );
-}
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(18, 2, 18, 4),
+                  child: Text(
+                    changeDateTimeFormat(message.timeSent.toDate()),
+                    style: const TextStyle(
+                      fontSize: 13.0,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Theme.of(context).backgroundColor,
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 14.0,
-            ),
-            MyChatTitleCard(attr1: _currentRoom),
-            const SizedBox(
-              height: 8.0,
-            ),
-            Expanded(
-              child: ListView.builder(
+    if (_messages.isEmpty) {
+      return const LoadingCircle();
+    } else {
+      return Scaffold(
+        body: Container(
+          color: Theme.of(context).backgroundColor,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 14.0,
+              ),
+              MyChatTitleCard(attr1: _currentRoom),
+              const SizedBox(
+                height: 8.0,
+              ),
+              Expanded(
+                  child: ListView.builder(
                 reverse: true,
                 itemCount: _messages.length,
                 itemBuilder: (BuildContext context, int index) {
                   ChatMessage message = _messages[index];
                   return FutureBuilder<String>(
                     future: getUsername(),
-                    builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                    builder:
+                        (BuildContext context, AsyncSnapshot<String> snapshot) {
                       if (snapshot.connectionState == ConnectionState.waiting) {
                         return CircularProgressIndicator();
                       }
@@ -213,49 +217,47 @@ Widget doMessageBubble(ChatMessage message, String username) {
                       }
                       String username = snapshot.data ?? '';
                       return doMessageBubble(message, username);
-                        },
-                      );
                     },
-                  )
-            ),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _textController,
-                      decoration: const InputDecoration(
-                        hintText: 'Enter a message',
+                  );
+                },
+              )),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _textController,
+                        decoration: const InputDecoration(
+                          hintText: 'Enter a message',
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 16.0),
-                  ElevatedButton(
-                    onPressed: () async {
-                      if (_textController.text.isNotEmpty) {
-                        _handleSubmitted(ChatMessage(
-                          senderName: await getUsername(),
-                          messageContent: _textController.text,
-                          timeSent: Timestamp.now(),
-                          roomId: _currentRoom,
-                          photoURL: profilePic,
-                        ));
-                      }
-                    },
-                    child: const Icon(
-                      IconData(
-                        0xe571, 
-                        fontFamily: 'MaterialIcons', 
-                        matchTextDirection: true),
-                      )),
-                ],
+                    const SizedBox(width: 16.0),
+                    ElevatedButton(
+                        onPressed: () async {
+                          if (_textController.text.isNotEmpty) {
+                            _handleSubmitted(ChatMessage(
+                              senderName: await getUsername(),
+                              messageContent: _textController.text,
+                              timeSent: Timestamp.now(),
+                              roomId: _currentRoom,
+                              photoURL: profilePic,
+                            ));
+                          }
+                        },
+                        child: const Icon(
+                          IconData(0xe571,
+                              fontFamily: 'MaterialIcons',
+                              matchTextDirection: true),
+                        )),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
-
 }
